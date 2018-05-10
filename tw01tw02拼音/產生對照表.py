5d1b715026f20f60cde47fb2bef1745faf1e896d
薛丞宏 <ihcaoe@gmail.com>
2018-05-10 18:05:14 +0800

    寫好的程式佮轉出來的檔案

---
+++ tw01tw02拼音/產生對照表.py
@@ -0,0 +1,24 @@
+
+import csv
+from 臺灣言語工具.音標系統.閩南語.通用拼音音標 import 通用拼音音標
+
+
+with open('1107通用.txt', 'rt') as txt檔案:
+    with open('tw01tw02對照表.csv', 'wt') as csv檔案:
+        writer = csv.DictWriter(
+            csv檔案, fieldnames=[
+                '通用', '臺羅',
+            ]
+        )
+        writer.writeheader()
+        for 通用一逝 in txt檔案.readlines():
+            通用無調 = 通用一逝.split()[0]
+            try:
+                臺羅 = 通用拼音音標(通用無調 + '1').轉換到臺灣閩南語羅馬字拼音().rstrip('18')
+            except AttributeError:
+                臺羅 = None
+            資料 = {
+                '通用': 通用無調,
+                '臺羅': 臺羅
+            }
+            writer.writerow(資料)
